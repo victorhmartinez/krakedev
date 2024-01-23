fd31bc5917296b861d3e997f8f2d54b89c02fd87
unknown <victormartinezg1998@gmail.com>
2024-01-23 14:39:38 -0500

    Boton cambia la imagen

--- Ejercicio Git/ejercicio.js
+++ Ejercicio Git/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
+    let cmpImgSaludo;
+    cmpImgSaludo = document.getElementById("imgSaludo")
+    cmpImgSaludo.src="./imagenes/b6XT7u.gif"
     console.log("Ingreso a saludar")
 }
