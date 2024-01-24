52ac14102aa2599d248b9c42238ceae3c0e11e7f
unknown <victormartinezg1998@gmail.com>
2024-01-23 22:31:55 -0500

    Funcion mostrar imagen completa

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -4,14 +4,21 @@
 
     let edad= recuperarInt("txtEdad");
     let estatura= recuperarFloat("txtEstatura");
-    let mensajeBienvenida= "Bienvenido"+nombre+" "+apellido
-    mostrarTexto("lblResultado",mensajeBienvenida)
+    let mensajeBienvenida= "Bienvenido "+nombre+" "+apellido;
+
+    mostrarTexto("lblResultado",mensajeBienvenida);
+    mostrarImagen("imgSaludo","./imagenes/saludo.gif")
  }
  mostrarTexto= function(idComponente,mensaje){
 let componente;
 componente=document.getElementById(idComponente);
 componente.innerText=mensaje;
  }
+ mostrarImagen= function(idComponente,rutaImagen){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.src=rutaImagen;
+     }
  recuperarTexto= function(idComponente){
     let componente;
     let valorIngresado
