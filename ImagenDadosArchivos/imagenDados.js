a120c02d49d42227df47c791f2470f5498fcca47
unknown <victormartinezg1998@gmail.com>
2024-01-26 12:09:09 -0500

    Implementacion de condicionales para presentar el promedio

--- ImagenDadosArchivos/imagenDados.js
+++ ImagenDadosArchivos/imagenDados.js
@@ -1,29 +1,66 @@
+let puntos = 0;
+let lanzamientos = 5;
+
 jugar=function(){
     let resultado;
     resultado=lanzarDado();
+    mostrarCara(resultado);
     console.log(resultado);
-    mostrarCara(resultado)
-   
+    modificarPuntos(resultado);
+    
 }
-// Funcion mostar cara, recibe el numero que quiere mostrar
-//Muestra la imagen correspondiente
-mostrarCara= function(numero){
-if(numero==1){
-    cambiarImagen("imgDado","dados1.png")
-}else if(numero==2){
-    cambiarImagen("imgDado","dados2.png")
-}else if(numero==3){
-    cambiarImagen("imgDado","dados3.png")
-}else if(numero==4){
-    cambiarImagen("imgDado","dados4.png")
-}else if(numero==5){
-    cambiarImagen("imgDado","dados5.png")
+modificarPuntos=function(numero){
+    puntos = puntos+numero;
+    cambiarTexto("lblPuntos",puntos);
+    //si el jugador obtiene mas de 20 puntos mostrar en pantalla un mensaje ganaste
+    if(puntos>20){
+        cambiarTexto("lblMensaje","GANASTE");
+        modificarLanzamientos();
+    }else{
+        modificarLanzamientos();
+    }
 }
-else if(numero==6){
-    cambiarImagen("imgDado","dados6.png")
+//no recibe parametros
+//resta 1 a la variable lanzamiento,guarda el resultado en la misma variable 
+//si lanzamientos llega a 0 mostrar en pantalla el mensaje GAME OVer
+modificarLanzamientos=function(){
+    lanzamientos =lanzamientos-1;
+    cambiarTexto("lblLazamientos",lanzamientos);
+    
+    if(lanzamientos==0 && puntos<20){
+        cambiarTexto("lblMensaje","GAME OVER");
+        
+    }else if(lanzamientos<0 ){
+        limpiar();
+    }
 }
 
-
+limpiar = function(){
+    //colocar puntaje en 0 y lanzamientos en 5
+    cambiarTexto("lblMensaje","");
+    cambiarTexto("lblPuntos",0);
+    cambiarTexto("lblLazamientos",5);
+    cambiarImagen("imgDado","");
+    puntos = 0;
+    lanzamientos = 5;
+}
+//funcion mostrarCara, recibe el numero que quier mostrar
+// muestra la imagen correspondiente al numero que recibe
+//no retorna nada
+mostrarCara=function(numero){
+    if(numero==1){ // == se compara = se asigna
+        cambiarImagen("imgDado","dados1.png");
+    }else if(numero==2){
+        cambiarImagen("imgDado","dados2.png");
+    }else if(numero==3){
+        cambiarImagen("imgDado","dados3.png");
+    }else if(numero==4){
+        cambiarImagen("imgDado","dados4.png");
+    }else if(numero==5){
+        cambiarImagen("imgDado","dados5.png");
+    }else{
+        cambiarImagen("imgDado","dados6.png");
+    }
 }
 
 lanzarDado=function(){
--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -4,6 +4,7 @@ calcularPromediosNotas = function () {
     let Nota3;
     let promedio;
     let mensaje;
+    let estado
     Nota1 = recuperarFlotante("txtNota1");
     Nota2 = recuperarFlotante("txtNota2");
     Nota3 = recuperarFlotante("txtNota3");
@@ -11,9 +12,19 @@ calcularPromediosNotas = function () {
     promedio=promedio.toFixed(2);
     mensaje="El promedio es "+promedio
     cambiarTexto("lblResultado",mensaje)
-    if(promedio>7){
-        cambiarImagen("cmpImagen","./imagenes/exito.gif")
-    }else{
-        cambiarImagen("cmpImagen","./imagenes/fracaso.gif")
-    }
+  if( promedio < 5 && promedio>0){
+    estado="REPROBADO";
+    cambiarImagen("cmpImagen","./imagenes/Reprobado.gif")
+  }else if(promedio>=5 && promedio<=8){
+    estado="BUEN TRABAJO";
+    cambiarImagen("cmpImagen","./imagenes/BuenTrabajo.gif")
+  } else if(promedio >8 && promedio <=10){
+    estado="EXCELENTE";
+    cambiarImagen("cmpImagen","./imagenes/excelente.gif")
+  } else{
+    estado="DATOS INCORRECTOS";
+    cambiarImagen("cmpImagen","./imagenes/DatosIncorrectos.gif") 
+  }
+  cambiarTexto("lblMensaje",estado)
+  
 }
