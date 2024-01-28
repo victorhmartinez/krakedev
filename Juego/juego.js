a5547fe1f9a1b2c6bd16558fc20e1eb4790ce6a2
unknown <victormartinezg1998@gmail.com>
2024-01-28 12:13:34 -0500

    Funcionalidad nueva partida impletada reto 36 terminado

--- Juego/juego.js
+++ Juego/juego.js
@@ -1,13 +1,35 @@
-jugar= function(seleccionado){
-    let elemento= generarElemento();
-    let ruta= generarRuta(elemento);
-    mostrarImagen("imgComputador",ruta)
-    let ganador= determinarGanador(seleccionado,elemento);
-    if(ganador==0){
-        mostrarTexto("lblGanador","EMPATE")
-    }else if(ganador==1){
-        mostrarTexto("lblGanador","GANASTE LA PARTIDA")
-    }else{
-        mostrarTexto("lblGanador","PERDISTE LA PARTIDA")
+let puntosUsuarios=0;
+let puntosComputador=0;
+jugar = function (seleccionado) {
+    let elemento = generarElemento();
+    let ruta = generarRuta(elemento);
+    mostrarImagen("imgComputador", ruta)
+    let ganador = determinarGanador(seleccionado, elemento);
+    if (ganador == 0) {
+        mostrarTexto("lblGanador", "EMPATE")
+    } else if (ganador == 1) {
+        mostrarTexto("lblGanador", "GANASTE LA PARTIDA")
+        puntosUsuarios+=1;
+        mostrarTexto("lblJuagdor", puntosUsuarios)
+    } else {
+        mostrarTexto("lblGanador", "PERDISTE LA PARTIDA")
+        puntosComputador+=1;
+        mostrarTexto("lblComputador", puntosComputador)
     }
+
+    if(puntosUsuarios==5){
+        mostrarTexto("lblResultado", "HAS GANADO EL JUEGO")
+    }else if(puntosComputador==5){
+        mostrarTexto("lblResultado", "El computador te ha vencido")
+    }
+}   
+
+nuevaPartida= function(){
+    puntosComputador=0;
+    puntosUsuarios=0;
+    mostrarTexto("lblResultado", "");
+    mostrarTexto("lblGanador", "");
+    mostrarTexto("lblComputador", "0");
+    mostrarTexto("lblJuagdor", "0");
+    mostrarImagen("imgComputador", '')   
 }
