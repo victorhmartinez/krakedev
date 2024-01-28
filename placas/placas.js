5fb715c02ffd4c1dc5f50591b51681e1897b8509
unknown <victormartinezg1998@gmail.com>
2024-01-27 23:28:50 -0500

    Funcion obtnerProvincia implementada y funcionando

--- placas/placas.js
+++ placas/placas.js
@@ -1,9 +1,16 @@
 validarPlaca=function(){
     let placa = recuperarTexto("txtPlaca");
+    let provincia;
     let erroresEstructura= validarEstructura(placa);
 
     if(erroresEstructura ==null){
-        mostrarTexto("lblResultado","ESTRUCTURA VALIDA")
+        mostrarTexto("lblResultado","ESTRUCTURA VALIDA");
+        provincia=obtenerProvincia(placa);
+        if(provincia!=null){
+            mostrarTexto("lblProvincia","Provincia: "+provincia); 
+        }else{
+            mostrarTexto("lblErrores","Provincia incorrecta"); 
+        }
     }else{
         mostrarTexto("lblResultado","ESTRUCTURA INVALIDA")
         mostrarTexto("lblErrores",erroresEstructura)
--- placas/servicioPlacas.js
+++ placas/servicioPlacas.js
@@ -1,11 +1,11 @@
 validarEstructura = function (placa) {
-    
+
     let errores = "";
     let tamanioPlaca = placa.length;
     if (tamanioPlaca !== 7 && tamanioPlaca !== 8) {
         errores += "La placa debe tener 7 u 8 caracteres.";
     } else {
-        errores +=  validarMayuscula(placa.charAt(0), "El primer caracter debe ser una letra mayúscula.");
+        errores += validarMayuscula(placa.charAt(0), "El primer caracter debe ser una letra mayúscula.");
         errores += validarMayuscula(placa.charAt(1), "El segundo caracter debe ser una letra mayúscula.");
         errores += validarMayuscula(placa.charAt(2), "El tercer caracter debe ser una letra mayúscula.");
         if (!esGuion(placa.charAt(3))) {
@@ -19,28 +19,79 @@ validarEstructura = function (placa) {
         }
     }
     console.log(errores)
-    if(errores.length>0){
+    if (errores.length > 0) {
         return errores
-    }else{
+    } else {
         return null
     }
 }
 validarMayuscula = function (digito, mensaje) {
     if (!esMayuscula(digito)) {
         return mensaje + "\n";
-    }else{
+    } else {
         return ""
     }
 }
 validarDigito = function (digito, mensaje) {
     if (!esDigito(digito)) {
         return mensaje + "\n ";
-    }else{
+    } else {
         return ""
     }
 }
 
-
+obtenerProvincia = function (placa) {
+    let primerDigito = placa.charAt(0);
+    if (primerDigito == "A") {
+        return "Azuay";
+    } else if (primerDigito == "B") {
+        return "Bolívar";
+    } else if (primerDigito == "U") {
+        return "Cañar";
+    } else if (primerDigito == "C") {
+        return "Carchi";
+    } else if (primerDigito == "X") {
+        return "Cotopaxi";
+    } else if (primerDigito == "H") {
+        return "Chimborazo";
+    } else if (primerDigito == "O") {
+        return "El Oro";
+    } else if (primerDigito == "E") {
+        return "Esmeraldas";
+    } else if (primerDigito == "W") {
+        return "Galápagos";
+    } else if (primerDigito == "G") {
+        return "Guayas";
+    } else if (primerDigito == "I") {
+        return "Imbabura";
+    } else if (primerDigito == "L") {
+        return "Loja";
+    } else if (primerDigito == "R") {
+        return "Los Ríos";
+    } else if (primerDigito == "M") {
+        return "Manabí";
+    } else if (primerDigito == "V") {
+        return "Morona Santiago";
+    } else if (primerDigito == "N") {
+        return "Napo";
+    }  else if (primerDigito == "S") {
+        return "Pastaza";
+    } else if (primerDigito == "P") {
+        return "Pichincha";
+    }else if (primerDigito == "K") {
+        return "Sucumbios";
+    } else if (primerDigito == "Q") {
+        return "Orellana";
+    } else if (primerDigito == "Y") {
+        return "Santa Elena";
+    } else if (primerDigito == "T") {
+        return "Tungurahua";
+    } else if (primerDigito == "Z") {
+        return "Zamora Chinchipe";
+    }else{
+        return null;
+    }
+}
 
 
 
