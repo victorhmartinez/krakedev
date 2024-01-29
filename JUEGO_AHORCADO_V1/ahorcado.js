cd4d9a95e6224f117a6cb5373b6451b0909bb845
unknown <victormartinezg1998@gmail.com>
2024-01-28 21:37:13 -0500

    Paso 0 completado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,1 +1,9 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function(caracter){
+    let ascii = caracter.charCodeAt(0);
+    if (ascii >= 65 && ascii <= 90) {
+        return true;
+    } else {
+        return false;
+    }
+}
