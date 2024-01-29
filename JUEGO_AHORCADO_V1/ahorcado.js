1c458a312852563b9e34b3ae6670f2374ec62dca
unknown <victormartinezg1998@gmail.com>
2024-01-28 22:41:10 -0500

    Paso 5 completo

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,6 +1,9 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 //Variables globales
 let palabraSecreta = "";
+let intentos=0;
+let coincidencias=0;
+let errores=0;
 
 //Funcion para verificar si la palabra es mayuscula
 esMayuscula = function (caracter) {
@@ -40,21 +43,37 @@ mostrarLetra = function (letra, posicion) {
 //Funcion para validar si la letra en la palabra secreta
 validar = function (letra) {
     let letrasEncontradas = 0;
-    for (let i = 0; i < palabraSecreta.length; i++) {
-        //Verificamos si la letra se encuntra dentro de la palabra secreta
-        if (palabraSecreta.charAt(i) == letra) {
-            //Mostramos la letra en caso de encontrala
-            mostrarLetra(letra, i);
-            letrasEncontradas++;
+   
+        for (let i = 0; i < palabraSecreta.length; i++) {
+            //Verificamos si la letra se encuntra dentro de la palabra secreta
+            if (palabraSecreta.charAt(i) == letra) {
+                //Mostramos la letra en caso de encontrala
+                mostrarLetra(letra, i);
+                letrasEncontradas++;
+                coincidencias++;
+            }
+        }
+        if(letrasEncontradas==0){
+            alert("LA LETRA NO ES PARTE DE LA PALABRA")
+            errores++;
         }
     }
-}
+   
+ 
+
+
 // Funcion para recupera la letra ingresada por el usuario
 ingresarLetra = function () {
     let letra = recuperarTexto("txtLetra");
+    intentos++;
     //Comprobamos si es mayuscula
     if(esMayuscula(letra)){
         validar(letra)
+        if(coincidencias==5){
+            alert("HAS GANADO")
+        }else if(intentos==10){
+            alert("HAS PERDIDO")
+        }
     }else{
         alert("SOLO SE ACEPTAN MAYUSCULAS")
     }
