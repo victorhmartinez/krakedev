beb26d18cdd4289847c04c562e2f8a434069d58b
unknown <victormartinezg1998@gmail.com>
2024-01-28 21:49:00 -0500

    Funcion guardarPalabra implementada y funcionando Paso 1 completo

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,9 +1,35 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+//Variables globales
+let palabraSecreta="";
+
+//Funcion para verificar si la palabra es mayuscula
 esMayuscula = function(caracter){
     let ascii = caracter.charCodeAt(0);
+    //verificacmos el codigo ASCCI de las mayusculas
     if (ascii >= 65 && ascii <= 90) {
         return true;
     } else {
         return false;
     }
+}
+//Funcion para guardar la palabra secreta
+guardarPalabra = function(){
+  let palabra=recuperarTexto("txtSecreta");
+  let noEsMayuscula=false;
+  //Comprobamos el tamaño de la palabra
+  if(palabra.length==5){
+    for(let i=0; i<palabra.length;i++){
+        if(!esMayuscula(palabra.charAt(i))){
+            noEsMayuscula=true;
+        }
+    }
+    //Comprobamos si existio alguna palabra q no es mayuscula
+    if(noEsMayuscula){
+        alert("Debe ingresar una palabra de 5 letras mayúsculas.")
+    }else{
+        palabraSecreta=palabra;
+    }
+    console.log(palabra);
+  }
+
 }
