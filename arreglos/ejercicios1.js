3b7a6bafe26eb414c2c1a7d83c61f55871ba8a35
unknown <victormartinezg1998@gmail.com>
2024-01-29 15:44:27 -0500

    Creando tablas con js y ejercicios con for

--- arreglos/ejercicios1.js
+++ arreglos/ejercicios1.js
@@ -6,14 +6,31 @@ agregarElementos = function () {
     notas.push(10);
     console.log(notas.length);
 }
-recorrerArreglo=function(){
+recorrerArreglo = function () {
     let notaR;
-    for(let indice=0;indice<notas.length;indice++){
-        notaR=notas[indice];
+    for (let indice = 0; indice < notas.length; indice++) {
+        notaR = notas[indice];
         console.log(notaR)
     }
 }
 
+generaTabla = function () {
+    let contenidoTabla = "";
+    let cmpTabla = document.getElementById("divTabla")
+    contenidoTabla += "<table><tr><td>UNO</td></tr>  <tr><td>DOS</td></tr></table>"
+    cmpTabla.innerHTML = contenidoTabla
+}
+mostrarNotas = function () {
+    let cmpTabla = document.getElementById("divTabla")
+    let contenidoTabla = "<table> <tr><th>NOTA</th></tr>";
+    let miNota;
+    for (let i = 0; i < notas.length; i++) {
+        miNota = notas[i]
+        contenidoTabla+="<tr><td>"+miNota+"</tr></td>"
+    }
+    contenidoTabla+= "</table>"
+    cmpTabla.innerHTML=contenidoTabla;
+}
 probarAgregar = function () {
     let notaRecuperada = recuperarInt("txtNota");
     agregarNota(notaRecuperada);
@@ -21,19 +38,20 @@ probarAgregar = function () {
 
 agregarNota = function (nota) {
     notas.push(nota);
+    mostrarNotas()
 }
 //Funcion para calcular el promediio
-calcularPromedio=function(){
-    let sumaNotas=0;
-    let promedio=0;
-   
-    for(let indice=0;indice<notas.length;indice++){
-       sumaNotas+=notas[indice]
+calcularPromedio = function () {
+    let sumaNotas = 0;
+    let promedio = 0;
+
+    for (let indice = 0; indice < notas.length; indice++) {
+        sumaNotas += notas[indice]
     }
-    promedio=sumaNotas/notas.length
+    promedio = sumaNotas / notas.length
     return promedio;
 }
-ejecutarPromedio=function(){
-    let promedio=calcularPromedio();
-    mostrarTexto("lblPromedio","El promedio es "+promedio)
+ejecutarPromedio = function () {
+    let promedio = calcularPromedio();
+    mostrarTexto("lblPromedio", "El promedio es " + promedio)
 }
